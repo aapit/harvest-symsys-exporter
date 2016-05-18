a211920845c79d22f48925a622864df408d8d90b
David Spreekmeester <david@grrr.nl>
2016-05-18 22:32:40 +0200

    Removed Department column

--- harvest-symsys-exporter.php
+++ harvest-symsys-exporter.php
@@ -57,7 +57,7 @@ $transformation->addCombinedColumn('Code', array(
 $sheet->removeColumns(array(
 	'Billable?', 'Invoiced?', 'Approved?', 'Employee?', 'Billable Rate',
 	'Billable Amount', 'Cost Rate', 'Cost Amount', 'Currency',
-	'First Name', 'Last Name', 'Project Code'
+	'First Name', 'Last Name', 'Project Code', 'Department'
 ));
 
 // Output as CSV.
